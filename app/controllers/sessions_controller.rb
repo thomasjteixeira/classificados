c337a768ce5cb44f4cf039f8ddd39b6024b7bd38
Thómas Jéfferson <thomas@debian-BULLSEYE-live-builder-AMD64>
2022-04-25 15:25:54 -0300

    added test to invalid user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params[:user][:email])
-        user.authenticate(params[:user][:password])
-        reset_session
-        session[:user_id] = user.id
-        redirect_to root_path, notice:"Bem vindo #{user.name}!"
-
+        if user && user.authenticate(params[:user][:password])
+            reset_session
+            session[:user_id] = user.id
+            redirect_to root_path, notice:"Bem vindo #{user.name}!"
+        else
+            redirect_to new_session_path, alert: "E-mail ou senha inválidos"
+        end
     end
 end
--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -1,17 +1,35 @@
 require "application_system_test_case"
 
 class LoginsTest < ApplicationSystemTestCase
+  def setup
+    @user = FactoryBot.create(:user, password: "password", password_confirmation: "password")
+  
+  end
+  
   test "login com credenciais validas" do
-    user = FactoryBot.create(:user, password: "password", password_confirmation: "password")
-
     visit root_path
     click_link "Entrar"
 
-    fill_in "E-mail", with: user.email
+    fill_in "E-mail", with: @user.email
     fill_in "Senha", with: "password"
 
     click_button "Entrar"
 
     assert_text "Bem vindo Darth Vader!"
+  end
+
+  test "login com credenciais invalidas" do
+    user = FactoryBot.create(:user, password: "password", password_confirmation: "password")
+    
+    visit root_path
+    click_link "Entrar"
+
+    fill_in "E-mail", with: @user.email
+    fill_in "Senha", with: "invalidpassword"
+
+    click_button "Entrar"
+
+    assert_equal new_session_path, current_path
+    assert_text "E-mail ou senha inválidos"
   end
 end
